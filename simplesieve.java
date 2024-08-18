79ce4455aa436aa9142e9309c32389349d62c09d
versus04 <shubhambind2004@gmail.com>
2024-08-18 21:42:29 +0530

    added simple sieve program

---
+++ simplesieve.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class simplesieve {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =  sc.nextInt();
+        boolean[] sieve = new boolean[n+1];
+        sieve[0]=sieve[1]=false;
+        for(int i=2;i<sieve.length;i++){
+            sieve[i]=true;
+        }
+        for(int i = 2;i<sieve.length;i++){
+                if(sieve[i]){
+                    for(int j=i*i;j<sieve.length;j+=i){
+                        sieve[j]=false;
+                    }
+                }
+        }
+        for(int i=2;i<sieve.length;i++){
+            if(sieve[i])System.out.println(i);
+        }
+    
+    
+    }
+    
+}
