2f3bde3c9a0d0ad793a20bb0587feff2ce6b99af
versus04 <shubhambind2004@gmail.com>
2024-08-20 23:42:50 +0530

    added

--- simplesieve.java
+++ simplesieve.java
@@ -1,25 +1,22 @@
 import java.util.*;
 public class simplesieve {
+
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n =  sc.nextInt();
+        int n = sc.nextInt();
         boolean[] sieve = new boolean[n+1];
         sieve[0]=sieve[1]=false;
-        for(int i=2;i<sieve.length;i++){
-            sieve[i]=true;
-        }
-        for(int i = 2;i<sieve.length;i++){
-                if(sieve[i]){
-                    for(int j=i*i;j<sieve.length;j+=i){
-                        sieve[j]=false;
-                    }
+        for(int i = 2 ; i<=sieve.length;i++){sieve[i]=true;}
+        for(int i = 2 ; i<=sieve.length; i++){
+            if (sieve[i]) {
+                for(int j = i*i ; j<=sieve.length ;j+=i){
+                    sieve[j] = false ; 
+
                 }
+            }
         }
-        for(int i=2;i<sieve.length;i++){
-            if(sieve[i])System.out.println(i);
-        }
-    
+    for(int i = 2; i<=sieve.length;i++){System.out.println(sieve[i]);}
     
     }
-    
 }
