9371cf3819071dd751f0dda74750409843ad6222
versus04 <shubhambind2004@gmail.com>
2024-08-21 00:09:29 +0530

    did twin primes count primes prime at specific location

--- simplesieve.java
+++ simplesieve.java
@@ -1,27 +1,39 @@
 import java.util.*;
 public class simplesieve {
 
-    static void sieve(int n){
+    static void sievecount(int n , int k){
+        int count = 0;
         boolean[] sieve = new boolean[n+1];
         sieve[0]=sieve[1]=false;
         for(int i = 2 ; i<sieve.length;i++){sieve[i]=true;}
         for(int i = 2 ; i<sieve.length; i++){
             if (sieve[i]) {
-                for(int j = i*i ; j<=sieve.length ;j+=i){
+                for(int j = i*i ; j<sieve.length ;j+=i){
                     sieve[j] = false ; 
 
                 }
             }
         }
-    for(int i = 2; i<sieve.length;i++){
-        if(sieve[i])System.out.println(i);
-        ;}
+            for(int i =2 ;i<sieve.length ; i++){
+                if(sieve[i]){
+                    count++;
+                    if(count==k){System.out.println(i); break;}
+                }
+
+            }
+
+            for(int i =2 ; i<sieve.length ; i++){
+                for(int j = i+1 ; j<sieve.length;j++){
+                        if(sieve[i] && sieve[j] && (j-i==2)){System.out.println( "(" +i +"," + j + ")");}
+                }
+            }
+       
 
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sieve(n);
+        sievecount(n  , 10);
         
     
     }
