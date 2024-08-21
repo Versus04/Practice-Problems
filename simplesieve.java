212622c7755a9b405a677a7db19f1cda06fbe267
versus04 <shubhambind2004@gmail.com>
2024-08-21 15:53:20 +0530

    small changes

--- simplesieve.java
+++ simplesieve.java
@@ -32,7 +32,9 @@ public class simplesieve {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
+        sc.close();
         sievecount(n  , 10);
         
     
