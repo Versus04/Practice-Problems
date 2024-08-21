5b856521f46b8e7558a1fefe62c47f8087638328
versus04 <shubhambind2004@gmail.com>
2024-08-21 15:24:30 +0530

    added code for eulers phi basic algo

---
+++ eulers.java
@@ -0,0 +1,16 @@
+public class eulers {
+    static int gcd(int a , int b ){
+            if(a==0)return b;
+            return gcd(b%a, a);
+    
+    }
+    static void eulers(int n ){
+        for(int i = 1;i<n;i++){
+            if(gcd(i, n)==1)System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        eulers(8);
+    }
+    
+}
