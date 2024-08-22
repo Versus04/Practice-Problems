1d691cc46fe8dc5e2b694963cbd81d3cc2200450
Versus04 <2shubhambind@gmail.com>
2024-08-22 12:48:36 +0530

    added

--- boothsalgo.java
+++ boothsalgo.java
@@ -1,4 +1,26 @@
+import java.util.*;
 public class boothsalgo {
-    
+    public static void main(String[] args) {
+    Scanner sc  = new Scanner(System.in);    
+        int m1 = sc.nextInt();
+
+        int m2 = sc.nextInt();
+        
+        int n = Integer.toBinaryString(m1).length();
+        System.out.println(n);
+        int p = 0;
+        for(int i = 0 ; i<n;i++)
+        {
+            int unit = (m1&0b1);
+            if (unit==1) {
+                p=p+m2;
+            }
+        m1 = m1>>>1;
+        m2=m2<<1;
+        
+
+        }
+        System.out.println(p);
+    }
     
 }
