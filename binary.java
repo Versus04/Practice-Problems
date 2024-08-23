9a79aa85d5e515c3f59967bddadf956bc6dc716a
Versus04 <2shubhambind@gmail.com>
2024-08-23 10:58:29 +0530

    deleted

--- binary.java
+++ binary.java
@@ -1,27 +1,13 @@
 public class binary {
-    public static String toBinary(int number) {
-        StringBuilder binary = new StringBuilder();
-        
-        if (number == 0) {
-            return "0";
-        }
-        
-        while (number > 0) {
-            int remainder = number % 2;
-            binary.append(remainder);
-            number = number / 2;
-        }
-        
-        return binary.reverse().toString();
-    }
+   
+
     static boolean palindrome(int N){
 
         String s =Integer.toBinaryString(N);
         int i = 0 , j = s.length()-1;
         while (i<j) {
             if (s.charAt(i)!=s.charAt(j)) {
-                return false ;
-                
+                return false ; 
             }
             i++;
             j--;
