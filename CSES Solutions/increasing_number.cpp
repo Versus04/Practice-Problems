b0d98883c901a6a2d9899349bd106f68fb99312d
Versus04 <2shubhambind@gmail.com>
2024-09-15 21:55:25 +0530

    added increasing number

--- CSES Solutions/increasing_number.cpp
+++ CSES Solutions/increasing_number.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-#include <bits/stdc++.h>
+
 int main() {
-    long long n;
-    cin >> n;
+    long long int n;
     long long turns = 0 ;
-    vector<int> arr(n);
-    for (long i = 0; i < n; i++) {
+    cin >> n;
+    vector<long long> arr(n);
+    for (long i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
-    
-    for(long i = 1; i < n; i++)
-   {
-    if(arr[i]<arr[i-1])
+    for (long i = 1; i < n; i++)
     {
-        turns += arr[i-1]-arr[i];
-        arr[i] = arr[i-1];   
+        if(arr[i]<arr[i-1])
+        {
+        turns += arr[i-1] - arr[i];
+            arr[i] = arr[i-1];
+        }
     }
-   }
-    cout<<turns<<endl;
+        cout<<turns<<endl;
     
-    
+
     return 0;
 }
