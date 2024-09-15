e812db7ca51d60fe2601876816240e59954bfe3f
Versus04 <2shubhambind@gmail.com>
2024-09-15 18:27:44 +0530

    optimised the code with mathematical formula

--- CSES Solutions/missing_number.cpp
+++ CSES Solutions/missing_number.cpp
@@ -4,23 +4,17 @@
 using namespace std;
 
 int main() {
-    int n;
+   long long int n;
     cin >> n;
-    vector<int> arr(n);  
-    int sum= 0  ; 
+    long long int sum= 0  ; 
     for (int i = 0; i < n-1; i++) {
         int input ; 
         cin >>input ;
-        arr[i] = input ;
         sum += input ;
 
     }
-    int realsum= 0 ;
-    for (int i = 1; i <= n ; i++)
-    {
-        realsum += i ;
-    }
-    cout<<realsum - sum <<endl;
+     long long int realsum = (n*(n+1))/2;
+         cout<<realsum - sum <<endl;
 
     return 0;
 }
